3e524266ebabf91a23983cde277f7184723d9b90
Daniel López <dankzer1@gmail.com>
2015-05-25 14:54:15 -0500

    Update (Live Archive) 5289 - Network Connections.cpp

--- (Live Archive) 5289 - Network Connections.cpp
+++ (Live Archive) 5289 - Network Connections.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <iostream>
+#include <cctype>
 using namespace std;
 int root[10000000];
 void init(int);
@@ -7,7 +8,7 @@ int find(int);
 bool joined(int,int);
 void join(int,int);
 int main() {
-	int test,ncomputers,a,b,successful,unsuccessful;
+	int test,ncomputers,a,b,successful,unsuccessful,r,t;
 	char form;
 	string line;
 	scanf("%d",&test);
@@ -18,9 +19,28 @@ int main() {
 		getline(cin,line);
 		while(getline(cin,line)) {
 			if(line.empty()) break;
-			a = line[2] - '0';
-			b = line[4] - '0';
 			form = line[0];
+			r = 1;
+			a = b = 0;
+			for(int i=line.size()-1;i>1;i--) {
+				if(line[i] == ' ') {
+					t = i - 1;
+					break;
+				}
+				if(isdigit(line[i])) {
+					b += ((line[i] - '0') * r);
+					r *= 10;
+				}
+			}
+			r = 1;
+			for(int j=t;j>0;j--) {
+				if(line[j] == ' ') break;
+				if(isdigit(line[j])) {
+					a += ((line[j] - '0') * r);
+					r *= 10;
+				}
+			}
+			printf("%d %d\n",a,b);
 			if(form == 'c') 
 				join(a,b);
 			else {
